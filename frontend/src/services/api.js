0070d9e8ac572667fd45eb3724d8b2af21dd1b2c
Temporary User <temp@example.com>
2025-06-12 05:15:23 +0300

    api deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // تكوين الإعدادات الأساسية للـ API
-const API_URL = 'http://127.0.0.1:8000/api';
-//const API_URL = 'http://159.223.177.1/api';
+//const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'http://159.223.177.1/api';
 
 const api = axios.create({
   baseURL: API_URL,
